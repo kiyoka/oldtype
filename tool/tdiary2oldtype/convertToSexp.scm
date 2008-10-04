39b2eafe57b4ed669e45802195dc8ede1c01376b
kiyoka <kiyoka@sumibi.org>
2008-10-04 09:28:23 +0000

    git-svn-id: http://svn.coderepos.org/share/lang/gauche/oldtype/trunk@20692 d0d07461-0603-4401-acd4-de1884942a52

--- tool/tdiary2oldtype/convertToSexp.scm
+++ tool/tdiary2oldtype/convertToSexp.scm
@@ -3,48 +3,53 @@
 (use srfi-1)
 
 
-(define (load-td2 port mode)
-  (let ((state 'title )
+(define (load-td2 port)
+  (let (
+        (body  '())
         (str-list (port->string-list port)))
+
+    (define (reset-body!)
+      (let1 ret (reverse body)
+            (set! body '())
+            (if (null? ret)
+                ""
+                (string-append "    ( \""
+                               (string-join ret "\n")
+                               "\" )"))))
+    (define (push-body! str)
+      (push! body str))
+
     (filter-map
      (lambda (str)
        (cond
         ((#/^TDIARY+/ str)
          #f)
         ((#/^[a-zA-Z-]+: / str)
-         (string-append
-          (if (#/^Date: / str)
-              "("
-              "")
-          (let1 pair (string-split str #/[ ]+/)
-                (format "(~a . ~a)" (car pair) (cadr pair)))))
+         (let1 pair (string-split str #/[ ]+/)
+               (string-append
+                (if (#/^Date: / str)
+                    (format "( \"~a\" . " (cadr pair))
+                    "")
+                (format "(~a . \"~a\")" (car pair) (cadr pair)))))
         ((= 0 (string-length str))
-         (set! state 'title)
-         #f)
+         (reset-body!))
         ((#/^.$/ str)
-         (set! state 'title)
-         ")")
+         (string-append (reset-body!)
+                        " )"))
         (else
          (let1 str (regexp-replace-all #/[\"]/ str "'")
-               (if (eq? 'c mode)
-                   (format "(body . \"~a\")" str))
-                   (case state
-                     ((title)
-                      (set! state 'body)
-                      (format "(title . \"~a\")" str))
-                     ((body)
-                      (format "(body . \"~a\")" str)))))))
+               (push-body! str)
+               #f))))
      str-list)))
 
 
 (define (main argv)
-  (print "(")
-  (for-each print
-            (load-td2
-             (current-input-port)
-             (string->symbol #?=(cadr argv))))
-  (print ")"))
-
+  (let1 mode (string->symbol (cadr argv))
+        (print "(")
+        (for-each print
+                  (load-td2
+                   (current-input-port)))
+        (print ")")))
 
 
   
